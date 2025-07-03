74c4934ae5a13c30befb94de1a41139dadaa35b7
Krishnadeshpande2907 <kd721921@gmail.com>
2025-07-03 08:37:13 +0530

    connected supabase

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,29 @@
+import {ReactNode} from "react";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+
+export default function RootLayer ({
+    children,
+    }: Readonly<{
+    children: React.ReactNode;
+    }>) {
+    // const geistSans = Geist({
+    //   variable: "--font-geist-sans",
+    //   subsets: ["latin"],
+    // });
+    
+    // const geistMono = Geist_Mono({
+    //   variable: "--font-geist-mono",
+    //   subsets: ["latin"],
+    // });
+    
+    // const inter = Inter({
+    //     variable: "--font-inter",
+    //     subsets: ["latin"],
+    // });
+    
+    return (
+    <>
+        {children}
+    </>
+    );
+}
